36161c690214d89608a75c458ea1d8b52da64d83
Tomas Berger <tomashb@stud.ntnu.no>
2020-07-04 12:56:50 +0200

    Add simple pages with info to user

--- webserver.py
+++ webserver.py
@@ -5,19 +5,26 @@ PORT = 2453 #Some Random port numbers
 FILE = "auth_code.file"
 class PySpoyServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        site = ""
         if(self.path[0] == "/"):
             index = self.path.find("code=")
             if index != -1:
                 code=self.path[index+5:]
                 with open(FILE, "w") as f:
                     f.write(code)
-
+                site = "You can close this window"
                 self.send_response(200)
-                self.end_headers()
+
             else:
+                self.send_response(418)
+                site = "Failed to read your authentication code!"
                 print("Failure to read the code")
         else:
+            site = "Spotify is being wired and send a wrong redirect"
+            self.send_response(404)
             print("Not an end point!")
+        self.end_headers()
+        self.wfile.write(bytes(site, "utf-8"))
 
 httpd = http.server.HTTPServer(("localhost", PORT), PySpoyServer)
 def runServer():
