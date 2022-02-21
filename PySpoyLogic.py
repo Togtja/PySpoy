8c31fcf4eb827752b81c1f2aaa602a9e4dadd5eb
Tomas Berger <tomashimberg.berger@technipfmc.com>
2022-02-21 16:51:33 +0100

    Add offline controls

--- PySpoyLogic.py
+++ PySpoyLogic.py
@@ -8,6 +8,13 @@ import PySpoyWebserver as webserver# Own lib
 class PySpoy():
     def __init__(self):
         self.listen_key = True
+        
+
+        self.key_control = keyboard.Controller()
+        self.run_on_release = set() #The function to run when the keys are released
+        self.pause_listen = False
+
+
         self.player_url = "https://api.spotify.com/v1/me/player/"
         with open(".config") as conf_file:
             self.clientID = conf_file.read()
@@ -40,7 +47,7 @@ class PySpoy():
         self.get_access_token()
 
         #TODO: make UI for this and load from file
-        self.keybind = {"Play/Pause": ([{keyboard.Key.alt_gr, keyboard.Key.ctrl_l, keyboard.Key.up}, {keyboard.Key.alt_gr, keyboard.Key.up}, {keyboard.Key.alt_r, keyboard.Key.ctrl_l, keyboard.Key.up}], self.playPause),
+        self.keybind = {"Play/Pause": ([{keyboard.Key.alt_gr, keyboard.Key.ctrl_l, keyboard.Key.up}, {keyboard.Key.alt_gr, keyboard.Key.up}, {keyboard.Key.alt_r, keyboard.Key.ctrl_l, keyboard.Key.up}, {keyboard.Key.alt, keyboard.Key.ctrl, keyboard.Key.up}], self.playPause),
         "SkipTrack": ([{keyboard.Key.alt_gr, keyboard.Key.ctrl_l, keyboard.Key.left}, {keyboard.Key.alt_gr, keyboard.Key.left}, {keyboard.Key.alt_r, keyboard.Key.ctrl_l, keyboard.Key.left}], self.skip_song), 
         "PreviousTrack": ([{keyboard.Key.alt_gr, keyboard.Key.ctrl_l, keyboard.Key.right}, {keyboard.Key.alt_gr, keyboard.Key.right}, {keyboard.Key.alt_r, keyboard.Key.ctrl_l, keyboard.Key.right}], self.prev_song), 
         "Quit": ([{keyboard.Key.shift, keyboard.Key.esc}], self.quit)}
@@ -72,6 +79,9 @@ class PySpoy():
                
     #TODO: Find a better way to know if state is play or pause, to minimize calls
     def playPause(self):
+        self.key_control.press(keyboard.Key.media_play_pause)
+        self.key_control.release(keyboard.Key.media_play_pause)
+        """
         playing = self.is_playing()
         if(playing == None):
             print("Failed to get 200 response from 'is_playing'")
@@ -82,6 +92,7 @@ class PySpoy():
         else:
             code = self.current_r.put(url=self.player_url+"play")
             print("play code: ", code.status_code)
+        """
 
     def is_playing(self):
         code = self.current_r.get(self.player_url+"currently-playing")
@@ -94,9 +105,13 @@ class PySpoy():
         return res
 
     def skip_song(self):
+        self.key_control.press(keyboard.Key.media_next)
+        self.key_control.release(keyboard.Key.media_next)
         pass
     
     def prev_song(self):
+        self.key_control.press(keyboard.Key.media_previous)
+        self.key_control.release(keyboard.Key.media_previous)
         pass
 
     def press_comb(self, key, binding : str):
@@ -106,10 +121,8 @@ class PySpoy():
         for combo in self.keybind[binding][0]:
             #TODO: Use intersection to fix bug where a key randomly get stuck in the set
             if (combo == self.cur_key):
-                self.keybind[binding][1]()
-            
-        return False
-    
+                return self.keybind[binding][1]
+
     def on_press(self, key):
          #Fix for some keyboards
         if(key.__str__() == "<65027>"):
@@ -118,14 +131,26 @@ class PySpoy():
         print("The key:", key.__str__())
         print("Keys pressed down: ", key, self.cur_key)
         for keyShort in self.keybind:
-            self.press_comb(key, keyShort)
+            func = self.press_comb(key, keyShort)
+            if func is not None:
+                self.run_on_release.add(func)
         return self.listen_key
     
     def on_release(self, key):
+        if self.pause_listen:
+            return True
         if(key.__str__() == "<65027>"):
             key = keyboard.Key.alt_gr
         if key in self.cur_key:
             self.cur_key.remove(key)
+        
+        if len(self.run_on_release) > 0 and len(self.cur_key) == 0:
+            self.pause_listen = True
+            print("Running func")
+            for func in self.run_on_release:
+                func()
+            self.run_on_release.clear()
+            self.pause_listen = False
         return self.listen_key
 
     def add_key_binding(self, binding, keys : set):
